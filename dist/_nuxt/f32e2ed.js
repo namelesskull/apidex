ca88eb00ae5cbc36630058a74c63a88716a0600f
Khaqul Huda <khaqulhudaa@gmail.com>
2023-10-25 15:02:39 +0700

    dist

---
+++ dist/_nuxt/f32e2ed.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[9],{477:function(e,n,t){"use strict";t.r(n);var o={name:"ResponseHere"},r=t(74),component=Object(r.a)(o,(function(){this._self._c;return this._m(0)}),[function(){var e=this._self._c;return e("div",[e("p",[this._v("Response goes here!")])])}],!1,null,null,null);n.default=component.exports}}]);
--- nuxt.config.js
+++ nuxt.config.js
@@ -72,4 +72,5 @@ export default {
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {},
   ssr: 'false',
+  target: 'static',
 }
